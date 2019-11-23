f7d85395a053838dad1e00821e5ff0670029b2a7
Zubarev Sergey <barclays13@mail.ru>
2019-11-23 16:19:26 +0300

    react-router

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import cartIcon from './shopping-cart-solid.svg';
+import {Link} from 'react-router-dom';
 import './app-header.scss';
 
 const AppHeader = ({total}) => {
     return (
-        <header className="header">
-            <a className="header__link" href="#">
-                Menu
-            </a>
-            <a className="header__link" href="#">
+        <header className="header" >
+            <Link to='/menu' className="header__link">
+            Menu
+            </Link>
+            <Link to='/cart' className="header__link">
                 <img className="header__cart" src={cartIcon} alt="cart"></img>
                 Total: {total} $
-            </a>
+            </Link>
         </header>
     )
 };
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,17 +2,19 @@ import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from '../hoc/';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Background from './food-bg.jpg';
 
 const App = ({RestoService}) => {
     console.log(RestoService.getMenuItems());
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
-        </div>
+        <Router>
+                <Route path='/'  component={() => <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app"></div>}/>
+                <AppHeader total={50}/>
+                <Route path='/menu' component={MainPage}/>
+                <Route path='/cart' component={CartPage}/>
+
+        </Router>
     )
 }
 
