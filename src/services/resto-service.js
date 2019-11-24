954ae8cd73920b6b9c8af2b6000c9fecad1c43e3
Zubarev Sergey <barclays13@mail.ru>
2019-11-24 15:11:20 +0300

    l15

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,20 @@
 export default class RestoService {
-    getMenuItems () {
-        return [];
+
+    constructor() {
+        this._apiBase = 'http://localhost:3000';
     }
+
+    async getResource (url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+    
+        return await res.json();
+    }
+
+    getMenuItems () {
+        return this.getResource('/menu');
+    }
+
 }
