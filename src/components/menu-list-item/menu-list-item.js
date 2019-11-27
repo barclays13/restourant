a83a9b8c89379a1e208f6868f1b961ac63df3da3
Zubarev Sergey <barclays13@mail.ru>
2019-11-27 23:03:18 +0300

    laba16

--- src/components/menu-list-item/menu-list-item.js
+++ src/components/menu-list-item/menu-list-item.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import './menu-list-item.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FaMortarPestle, FaPizzaSlice , FaDrumstickBite, FaUntappd} from "react-icons/fa";
 
 const MenuListItem = ({menuItem}) => {
     const {title, price, url, category} = menuItem;
 
-    if (category === 'meat'){
-        console.log(0);
-    } 
+    let typeIcon;
+    if (category==='meat') {
+        typeIcon = <FaDrumstickBite/>
+    } else if (category==='pizza') {
+        typeIcon = <FaPizzaSlice/>
+    } else if (category==='salads') {
+        typeIcon = <FaMortarPestle/>
+    } else {
+        typeIcon = <FaUntappd/>
+    }
+
     return (
             <li className="menu__item">
                 <div className="menu__title">{title}</div>
                 <img className="menu__img" src={url} alt="Cesar salad"></img>
-                <div className="menu__category">Category: <span>{category}</span></div>
+                <div className="menu__category">Category: <span>{category}</span><div className="category-icon">{typeIcon}</div></div>
+                
                 <div className="menu__price">Price: <span>{price}$</span></div>
                 <button className="menu__btn">Add to cart</button>
+
             </li>
     )
-}
+};
+
+
 
 export default MenuListItem;
